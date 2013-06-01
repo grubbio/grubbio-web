34f9af904532c6cdd61daa26a6215fc8ff8cfc65
theef <k@dmcy.us>
2013-05-31 22:59:33 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrubbioWeb::Application.config.secret_token = '99162808cd7b140e02f1b7b36348fd744d9a6968796478b34998db6520704e45ccb325fd6c7fab03853afcd05cb1511fccc98806ab1a48dc5a863fced1aa103b'
