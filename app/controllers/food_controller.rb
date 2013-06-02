f6a1037d4079b435d537832fb3d2ca33b595a0dc
Jeff Sutherland <jsutherland@itriagehealth.com>
2013-06-01 22:52:31 -0600

    link businesses to food

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -6,6 +6,8 @@ class FoodController < ApplicationController
 
 	def show
 		@food = FoodProduct.find(params[:id])
+    ids = BusinessProfileFoodProduct.find_all_by_food_product_id(params[:id])
+    @businesses = Business.find(BusinessProfile.find(ids).map(&:id))
 	end
 
 end
