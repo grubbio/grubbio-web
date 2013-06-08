8513b2a9a9fe9e68a774a22edd140bc71024ff29
Jeff Sutherland <jefferey.sutherland@gmail.com>
2013-06-08 17:49:24 -0600

    throw up basic auth to keep out snoopers while we get organized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery
+  #protect_from_forgery
+
+  before_filter :authenticate
+
+  def authenticate
+    def authenticate
+      if Rails.env.production?
+        authenticate_or_request_with_http_basic do |username, password|
+          username == "tastytasty" && password == "br0cc0l1$"
+        end
+      end
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-
 	def index; end
 	def resources; end
-
 end
