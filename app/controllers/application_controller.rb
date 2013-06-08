f91d7ebd47ddd7debde71e95e47937b332f7f3e4
Jeff Sutherland <jefferey.sutherland@gmail.com>
2013-06-08 17:52:51 -0600

    double defnining a method is stupid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,9 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate
 
   def authenticate
-    def authenticate
-      if Rails.env.production?
-        authenticate_or_request_with_http_basic do |username, password|
-          username == "tastytasty" && password == "br0cc0l1$"
-        end
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "tastytasty" && password == "br0cc0l1$"
       end
     end
   end
